413188b0a3309fbde3dd617412a37b443858230d
YanaKidun <stabrovskayaya@gmail.com>
2023-02-01 23:02:36 +0300

    HW4

---
+++ HW 4_5.py
@@ -0,0 +1,17 @@
+""" Реализовать формирование списка, используя функцию range() и возможности генератора.
+В список должны войти четные числа от 100 до 1000 (включая границы).
+Необходимо получить результат вычисления произведения всех элементов списка.
+Подсказка: использовать функцию reduce()."""
+
+from functools import reduce
+
+new_list = [x for x in range(100, 1001) if x % 2 == 0]
+print(new_list)
+
+def custom_sum(first, second):
+    return first * second
+
+# result = reduce(custom_sum, new_list)
+# print(result)
+
+print(f'результат - {reduce(custom_sum,([x for x in range(100, 1001) if x % 2 == 0]))}')
